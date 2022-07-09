ee796ff15dbe43e65786561d5ccbce62cb8dd42c
Flux <flustixshinu@gmail.com>
2022-07-09 22:30:48 +0200

    woo

--- src/api/ApiRouter.ts
+++ src/api/ApiRouter.ts
@@ -6,8 +6,14 @@ export default class ApiRouter {
     public static init(): express.Router { 
         const router: express.Router = express.Router();
 
+        //maps
         router.route('/maps').get(ApiMaps.getMaps);
         router.route('/map/:id').get(ApiMaps.getMap);
+        
+        // users
+        router.route('/user/:id').get(ApiResponse.send404);
+
+        // 404
         router.route("*").get(ApiResponse.send404);
 
         return router;
