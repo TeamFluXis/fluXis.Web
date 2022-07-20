0b5307aa9e5523dbd61bd2b4a1fa0c64955033bb
Flux <flustixshinu@gmail.com>
2022-07-20 18:33:06 +0200

    some more routes to 404 cuz we dont have enough

--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -3,14 +3,23 @@ import Charts from "../pages/charts/Charts";
 
 import Home from "../pages/home/Home";
 import Map from "../pages/map/Map";
+import NotFound from "../pages/error/NotFound";
 
 export default class Router {
     public static InitializeRouter(): express.Router {
         const router: express.Router = express.Router();
 
         router.route('/').get(Home.GET);
+
         router.route('/charts').get(Charts.GET);
+
+        router.route('/map').get(NotFound.GET);
         router.route('/map/:id').get(Map.GetMap);
+
+        router.route('/user').get(NotFound.GET);
+        router.route('/user/:id').get(NotFound.GET);
+
+        router.route('/ranks').get(NotFound.GET);
 
         return router;
     }
